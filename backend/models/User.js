60e4b64c45a72d49b5eecd09547c54062e12d858
Paras Ningune <75624432+Paras-03@users.noreply.github.com>
2024-10-06 23:49:11 +0530

    final commit

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,8 +4,7 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true,
-        unique: true
+        required: true
     },
     password: {
         type: String,
